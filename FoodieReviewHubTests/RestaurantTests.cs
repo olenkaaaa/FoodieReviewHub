23f05d4b80db0a04cff7dbf5db34a4e1ffde4741
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-10-25 22:04:20 +0200

    Test statystyk

--- FoodieReviewHubTests/RestaurantTests.cs
+++ FoodieReviewHubTests/RestaurantTests.cs
@@ -5,15 +5,19 @@ namespace FoodieReviewHubTests
     public class RestaurantTests
     {
         [Test]
-        public void WhenRestaurantCollectsTwoScores_ShouldReturnCorrectResult()
+        public void WhenRestaurantGetStars_CheckIfStatisticsAreCorrect()
         {
             var restaurant = new Restaurant("Bistro");
             restaurant.AddStar(4);
             restaurant.AddStar(3);
+            restaurant.AddStar(5);
+            restaurant.AddStar(5);
 
-            var result = restaurant.Stars;
+            var statistics = restaurant.GetStatistics();
 
-            Assert.AreEqual(7, result);
+            Assert.AreEqual(3, statistics.Min);
+            Assert.AreEqual(5, statistics.Max);
+            Assert.AreEqual(4.25, statistics.Average);
         }
     }
 }
